419190c5e542b41a46cf3dec2823611ed8f0676d
PHinterbauer <phinterbauer.privat@gmail.com>
2026-02-20 12:45:16 +0100

    removed bs chatgpt template files and stuff in inni.py files 😭

--- src/adapters/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Adapters - Konkrete Implementierungen der Ports"""
-
-from .repository import InMemoryRepository, RepositoryFactory
-from .report import ConsoleReportAdapter
-
-__all__ = ["InMemoryRepository", "RepositoryFactory", "ConsoleReportAdapter"]
--- src/domain/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Domain Layer - Geschäftslogik und Entity-Modelle"""
-
-from .product import Product
-from .warehouse import Warehouse
-
-__all__ = ["Product", "Warehouse"]
